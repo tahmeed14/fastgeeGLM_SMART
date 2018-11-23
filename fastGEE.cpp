c7929a2477599a0af747b0e288d32f2a6c92cf4e
Tahmeed Tureen <tureen@umich.edu>
2018-11-23 18:11:05 -0500

    Initialize Repo

---
+++ fastGEE.cpp
@@ -0,0 +1,14 @@
+// Biostats 615 - Statistical Computing //
+// fastGEE.cpp - Tahmeed Tureen, Henghsi Yu, and Weiqi Zhou
+// Supplement Code for Final Project
+
+// Load typical packages
+#include<iostream>
+#include<iomanip>
+
+// Typiccal C libraries
+#include<cmath>
+#include<cstdlib>
+
+// Include Eigen Package for Matrices
+#include <Eigen/Core>
