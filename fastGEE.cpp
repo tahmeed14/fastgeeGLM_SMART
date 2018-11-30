defdc6b54313d2dd94c7489f71e157ec4d1abb32
Tahmeed Tureen <tureen@umich.edu>
2018-11-30 16:35:17 -0500

    11/30

--- fastGEE.cpp
+++ fastGEE.cpp
@@ -14,6 +14,7 @@
 // C++ Data Structures
 #include<set>
 #include<map>
+#include<vector>
 
 // Include Eigen Package for Matrices
 #include <Eigen/Core>
@@ -23,21 +24,6 @@
 
 using namespace std;
 // we avoid using namespace Eigen to be able to clearly see where we are calling the Eigen Package
-
-// Requires: A Eigen Matrix
-// Modifies: Nothing
-// Effects: Returns the number of unique observations (patients, study participants) in the data set
-
-template <typename any>
-int count_unique(const Eigen::EigenBase<any> &mat_in) {
-	std::set<double> unique_set;
-	for (int i = 0; i < int(mat_in.rows()); ++i) {
-		unique_set.insert( mat_in(i, 0) );
-	}
-
-	return int(unique_set.size());
-}
-
 
 // Driver Code
 int main(int argc, char* argv[]) {
@@ -62,6 +48,18 @@ int main(int argc, char* argv[]) {
 	read_Design.cloneToEigen(design_X);
 	read_Y.cloneToEigen(outcome_Y);
 
+	Eigen::MatrixXd Weights(outcome_Y.rows(), 1);
+	Weights.setOnes();
+
+	// cout << Weights.rows() << endl << endl;
+	// cout << Weights << endl;
+
+
+	if (int (argc) == 5) {
+		Matrix615<double> read_Weights;
+		read_Weights.readFromFile(argv[4]);
+	} 
+
 	// cout << design_X.rows() << " " << design_X.cols() << endl << endl;
 	// cout << design_X << endl << endl << endl;
 	// cout << outcome_Y.rows() << " " << outcome_Y.cols() << endl << endl;
@@ -71,23 +69,29 @@ int main(int argc, char* argv[]) {
 	Eigen::MatrixXd ID_Mat;
 	read_ID.cloneToEigen(ID_Mat);
 
-	std::set<int> unique_set; // to dynamically count n
+	std::set<int> unique_ID; // to dynamically count n
 	std::map<int, int> id_map; // to dynamically count m
+	std::map<int, double> weight_map;
 
-	for (int i = 0; i < int(outcome_Y.rows()); ++i) {
-		unique_set.insert(  ID_Mat(i,0) );
-		id_map[ID_Mat(i, 0)] += 1; 
+	for (int i = 0; i < int(ID_Mat.rows()); ++i) {
+		unique_ID.insert(  ID_Mat(i,0) );
+		id_map[ID_Mat(i, 0)] += 1;
+		weight_map[ID_Mat(i, 0)] = Weights(i, 0); 
 	}
 
-	int n = int( unique_set.size() );
-	int m = int( id_map[ID_Mat(1,0)] );
+	int n = int( unique_ID.size() );
+	// int m = int( id_map[ID_Mat(1,0)] );
 
 	// q = (p + 1) : # number of paramters to be estimated using GEE
 	int q = int ( design_X.cols() );
 
+	// Correlation Structure
+
+	// Step : Weights
+
 	// Step (3): Initialize betas *********** Should we incorporate the beta's that come from a fast linear regression or no?
 
-	Eigen::MatrixXd Betas_new(int(design_X.cols()) , 1); // dimensions (p+1)  x 1
+	Eigen::MatrixXd Betas_new(q , 1); // dimensions (p+1)  x 1
 	Betas_new.setZero();
 
 	// cout << "Betas ***********************" << endl;
@@ -95,7 +99,7 @@ int main(int argc, char* argv[]) {
 
 	// Step (4): Initialize rho and phi (rho: off diagonals of correlation structure & phi: )
 	double rho = 0.0;
-	double phi = 0.0;
+	// double phi = 0.0;
 
 	// Step (5): Set up variables for iteration convergence check
 
@@ -111,9 +115,13 @@ int main(int argc, char* argv[]) {
 	int iteration_count = 0;
 
 	// Step (7): Assign appropriate value to n*
-	double n_star = (0.5) * double(n) * double(m) * double(m - 1);
+	// double n_star = (0.5) * double(n) * double(m) * double(m - 1);
+
+	// double n_star = (0.5) * double(n) * double(5) * double(5 - 1);
 
 	// cout << " n* is " << n_star << endl;
+
+	double n_star_sum = 0.0;
 
 	// Step (8): Start the GEE Estimation
 
@@ -128,17 +136,17 @@ int main(int argc, char* argv[]) {
 		Betas_updated = Betas_new;
 
 		// (2) Vector EE ((p+1) x 1)
-		Eigen::MatrixXd EE( int(design_X.cols()), 1);
+		Eigen::MatrixXd EE(q , 1);
 		EE.setZero();
 
 		// (3) Matrix GI ( (p+1) x (p+1) )
 		// First derivative of EE; Model-based Variance
-		Eigen::MatrixXd GI( int(design_X.cols()) , int(design_X.cols()));
+		Eigen::MatrixXd GI(q , q);
 		GI.setZero();
 
 		// (4) Matrix G ( (p+1) x (p+1) )
 		// Meat of the Sandwhich Estimator
-		Eigen::MatrixXd G( int(design_X.cols()) , int(design_X.cols()));
+		Eigen::MatrixXd G(q , q);
 		G.setZero();		
 
 		// (5) Initialize phi(sum) and tau(sum)
@@ -152,8 +160,13 @@ int main(int argc, char* argv[]) {
 		// inner loop in while loop Time-Complexity: O(n^2)??
 
 		for (int i = 0; i < n; ++i) {
+
+			int m = id_map[i + 1];
 			start = end + 1;
 			end = start + m - 1;
+
+			//update n*
+			n_star_sum = n_star_sum + (0.5) * double(m) * double(double(m) - 1.0);
 
 			// assign mu_i ( m x (p+1) ) for ith person
 			// Eigen.block(starting_row = , starting_col = , dim_row = , dim_col = )
@@ -173,10 +186,10 @@ int main(int argc, char* argv[]) {
 			// create R matrix (m x m) for each observation/patient
 			Eigen::MatrixXd R(m, m);
 			R.setConstant(rho); // off-diagonals will be rho
-			for (int i = 0; i < int(m); ++i) R(i, i) = 1; //diagonals will be 1
+			for (int d = 0; d < int(m); ++d) R(d, d) = 1; //diagonals will be 1
 
 			// update EE ((p+1) x 1)
-			EE = EE + ((design_X.block(start, 0, m, q).transpose()) * (R.inverse()*r_i));
+			EE = EE + ((design_X.block(start, 0, m, q).transpose()) * (R.inverse() * (weight_map[i+1] * r_i) )); // weight_map
 
 			// update GI ((p+1) x (p+1)) or (q x q)
 			GI = GI + ((design_X.block(start, 0, m, q).transpose()) * R.inverse() *  design_X.block(start, 0, m, q));
@@ -193,7 +206,7 @@ int main(int argc, char* argv[]) {
 		diff_beta = (Betas_new - Betas_updated).norm();
 
 		// update rho
-		rho = ( (( double(n) - double(q) ) * tau_sum) / ( (double(n_star) - double(q) ) * phi_sum) );
+		rho = ( (( double(n) - double(q) ) * tau_sum) / ( (double(n_star_sum) - double(q) ) * phi_sum) );
 
 
 		sandwhich_Mat = GI.inverse() * G * GI.inverse();
@@ -201,13 +214,15 @@ int main(int argc, char* argv[]) {
 		iteration_count += 1;
 	}
 
-	cout << "Estimated Betas are: " << endl;
+	cout << endl << "Estimated Betas are: " << endl;
 	cout << Betas_new.transpose() << endl << endl << endl;
 
-	cout << "The sandwhich matrix is: " << endl << endl;
-	cout << sandwhich_Mat << endl;
-	 
+	// cout << "The sandwhich matrix is: " << endl << endl;
+	// cout << sandwhich_Mat << endl << endl;
 
+	cout << "The beta S.E.'s are: " << endl << endl;
+	cout << sandwhich_Mat.diagonal().array().sqrt() << endl << endl;
+	
 	return 0;
 
 }
